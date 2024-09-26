0a680c47e06fe69cca18ac12c0283c25b650d54f
ehsanHogo <ehsan.ho.go@gmail.com>
2024-09-26 12:36:15 +0330

    make showCoins Components

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,9 @@
 <script setup>
-import { computed, onMounted, ref } from "vue";
-import CoinCard from "./components/CoinCard.vue";
-
-const coins = ref();
-
-const filteredCoins = ref();
-
-const getData = async () => {
-  try {
-    const response = await fetch("https://api.exir.io/v2/constants");
-
-    const data = await response.json();
-
-    console.log(data.coins);
-
-    filteredCoins.value = Object.fromEntries(
-      Object.entries(data.coins).slice(0, 30)
-    );
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-onMounted(() => {
-  getData();
-});
+import ShowCoins from "./components/ShowCoins.vue";
 </script>
 
 <template>
-  <div class="showCoins">
-    <div v-for="coin in filteredCoins" :key="coin.id">
-      <CoinCard :name="coin.fullname" :logo-url="coin.logo"></CoinCard>
-    </div>
-  </div>
+  <ShowCoins></ShowCoins>
 </template>
 
-<style scoped>
-.showCoins {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  column-width: 50%;
-  gap: 1rem;
-}
-</style>
+<style scoped></style>
