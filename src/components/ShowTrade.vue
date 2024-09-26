8fde19e7466e4c344e201965c37f4347b1db7df1
ehsanHogo <ehsan.ho.go@gmail.com>
2024-09-26 15:40:01 +0330

    buld trade section

--- src/components/ShowTrade.vue
+++ src/components/ShowTrade.vue
@@ -1,21 +1,34 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import TradeCard from "./TradeCard.vue";
 const pairs = ref();
-const getPairs = async () => {
+
+const coins = ref();
+const getData = async () => {
   const res = await fetch("https://api.exir.io/v2/constants");
 
   const data = await res.json();
 
   console.log(data);
 
+  coins.value = data.coins;
   pairs.value = data.pairs;
+  console.log("find : ", pairs.value);
+};
 
-  console.log(pairs.value);
+const findLogo = (name) => {
+  let logo;
+  const result = Object.entries(coins.value).filter((key, value) => {
+    return key[0] === name;
+  });
+
+  console.log(result[0][1].logo);
+
+  return result[0][1].logo;
 };
 
 onMounted(() => {
-  getPairs();
+  getData();
 });
 </script>
 
@@ -23,27 +36,31 @@ onMounted(() => {
   <div>
     <h1 class="tradeList">Trade</h1>
 
-    <div class="pairCard">
-      <TradeCard></TradeCard>
+    <div v-for="pair in pairs" :key="pair.id" class="pairCards">
+      <TradeCard
+        :pairName="pair.name"
+        :pairOne="pair.pair_base"
+        :pairTwo="pair.pair_2"
+        :pairTwoLogo="findLogo(pair.pair_2)"
+        :pairOneLogo="findLogo(pair.pair_base)"
+        :pairMinPrice="pair.min_price"
+        :pairMaxPrice="pair.max_price"
+      ></TradeCard>
     </div>
   </div>
 </template>
 
 <style scoped>
-.pairName {
-}
-.pairCard {
+.pairCards {
   width: 100%;
 
-  background-color: aquamarine;
-
-  padding: 2rem;
-  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
 }
 
 .tradeList {
   padding: 1rem 0;
-
+  color: white;
   text-align: center;
 }
 </style>
--- src/components/TradeCard.vue
+++ src/components/TradeCard.vue
@@ -1,29 +1,92 @@
-<script setup></script>
+<script setup>
+defineProps({
+  pairName: String,
+  pairOne: String,
+  pairTwo: String,
+  pairMinPrice: Number,
+  pairMaxPrice: Number,
+  pairTwoLogo: String,
+  pairOneLogo: String,
+});
+</script>
 
 <template>
-  <div class="pairName">pair name</div>
-  <div class="tradeCard">
-    <div class="pairOne">pair one</div>
+  <div class="pairCard">
+    <div class="pairName">{{ pairName }}</div>
+    <div class="tradeCard">
+      <div class="pairOne">
+        <img class="coinLogo" :src="pairOneLogo" alt="logo" />
+        <p>{{ pairOne }}</p>
 
-    <div class="pairTwo">pair two</div>
+        
+      </div>
 
-    <div class="pairePrice">price</div>
+      <div class="pairTwo">
+        <img class="coinLogo" :src="pairTwoLogo" alt="logo" />
+
+        <p>{{ pairTwo }}</p>
+      </div>
+
+      <div class="pairPrice">{{ pairMinPrice }}$</div>
+      <div class="pairPrice">{{ pairMaxPrice }}$</div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.pairCard {
+  background-color: aquamarine;
+
+  padding: 1.5rem;
+
+  border-radius: 20px;
+
+  margin: 1rem;
+
+
+ 
+}
 .tradeCard {
   display: flex;
+
+  margin: 0.5rem;
+
+  gap: 10rem;
 }
 .pairOne {
+  display: flex;
   width: 25%;
+  align-items: center;
+  gap: 1rem;
+
 }
 
 .pairTwo {
   width: 25%;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
 }
 
 .pairPrice {
   width: 50%;
+
+  display: flex;
+  justify-content: start;
+  align-items: center;
+}
+
+.pairName {
+  padding-bottom: 1rem;
+
+  font-weight: bolder;
+}
+
+.coinLogo {
+  width: 50%;
+
+  display: flex;
+  justify-content: end;
 }
 </style>
