04f4face64744ec38edc9ca930ed02e14250d000
ehsanHogo <ehsan.ho.go@gmail.com>
2024-09-26 11:33:54 +0330

    make template and fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from "vue";
+import CoinCard from "./components/CoinCard.vue";
+
+const coins = ref();
+
+const getData = async () => {
+  try {
+    const response = await fetch("https://api.exir.io/v2/constants");
+
+    const data = await response.json();
+
+    console.log(data.coins);
+
+    coins.value = data.coins;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+onMounted(() => {
+  getData();
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="showCoins">
+    <div v-for="coin in coins" :key="coin.id">
+      <CoinCard :name="coin.fullname" :logo-url="coin.logo"></CoinCard>
+    </div>
+    <CoinCard></CoinCard>
+    <CoinCard></CoinCard>
+    <CoinCard></CoinCard>
+    <CoinCard></CoinCard>
+    <CoinCard></CoinCard>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.showCoins {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-width: 50%;
+  gap: 1rem;
 }
 </style>
---
+++ src/components/CoinCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps({
+  name: String,
+  logoUrl: String,
+});
+</script>
+
+<template>
+  <div class="coinCard">
+    <div class="coinName">{{ name }} name of coin</div>
+
+    <div class="coinLogo">
+      <img class="logoPic" :src="logoUrl" alt="Coin Logo" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.coinCard {
+  width: 100%;
+
+  display: flex;
+
+  background-color: aquamarine;
+  padding: 1rem 1rem;
+  border-radius: 10px;
+}
+
+.coinName {
+  width: 50%;
+}
+
+.coinLogo {
+  width: 50%;
+}
+
+.logoPic {
+  width: 25%;
+}
+</style>
